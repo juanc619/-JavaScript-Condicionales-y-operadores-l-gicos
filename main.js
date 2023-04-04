9cefe21ed09eddb70999ea3d32a5d352b641b0cb
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-04-04 14:00:35 -0500

    mod js

---
+++ main.js
@@ -0,0 +1,17 @@
+// ==           Es igual
+// ===          Es estrictamente igual
+// !=           Es distinto
+// !==          Es estrictamente distinto
+// < <= > >=    Menor / Mayor / Menor o igual / Mayor o igual
+
+// &&           Operador AND (y)
+// ||           Operador OR (o)
+// !            Operador NOT (no)
+
+let edad = 18;
+
+if (edad == 18) {
+    alert("Eres mayor de edad. Puedes ingresar")
+}else {
+    alert("Eres menor de edad. No puedes ingresar")
+}
