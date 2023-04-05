c98c2b908dec458fca11ee15d4c9ad358a7c907b
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-04-04 19:01:36 -0500

    mod main.js

--- main.js
+++ main.js
@@ -8,10 +8,39 @@
 // ||           Operador OR (o)
 // !            Operador NOT (no)
 
-let edad = 18;
+// let edad = prompt("Ingrese su edad");
 
-if (edad == 18) {
-    alert("Eres mayor de edad. Puedes ingresar")
-}else {
-    alert("Eres menor de edad. No puedes ingresar")
+// if (edad == 18) {
+//     alert("Eres mayor de edad. Puedes ingresar");
+// }else {
+//     alert("Eres menor de edad. No puedes ingresar");
+// }
+
+// let hora = prompt ("Ingrese la hora");
+
+// if (hora >=6 && hora <12) {
+//     alert("Buenos dias!");
+// }else if (hora >=12 && hora <20) {
+//     alert("Buenas tardes!");
+// }else {
+//     alert ("Buenas noches!");
+// }
+
+let usuario1 = "Juan";
+let usuario2 = "Carlos";
+
+let nombreUsuario = prompt ("Ingrese su nombre de usuario");
+
+
+if (nombreUsuario == usuario1 || nombreUsuario == usuario2) {
+    alert("Bienvenido " + nombreUsuario);
+} else {
+    alert ("Nombre de usuario incorrecto.");
 }
+// if (nombreUsuario == "Juan") {
+//     alert ("Bienvenido " + nombreUsuario);
+// }else if (nombreUsuario == "Carlos") {
+//     alert ("Bienvenido " + nombreUsuario);
+// }else {
+//     alert ("Nombre de usuario incorrecto");
+// }
